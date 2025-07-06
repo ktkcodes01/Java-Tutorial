d08f4439b387b229503bc41656af25db233e8030
ktkcodesforlife <ktkvlogs23@gmail.com>
2025-07-06 22:09:39 +0530

    Java Basics

---
+++ Arithmetic.java
@@ -0,0 +1,17 @@
+public class Arithmetic {
+
+    public static void main(String[] args) {
+        String a = "5";
+        String b = "8";
+
+        int num1 = Integer.parseInt(a);
+        int num2 =  Integer.parseInt(b);
+        System.out.println(" Additions : "+ (num1+num2));
+        System.out.println(" Subtraction : "+ (num1-num2));
+        System.out.println(" Multiplication : "+ (num1*num2));
+        System.out.println(" Division : "+ (num2/num1));
+        System.out.println(" Modulo : "+ (num2%num1));
+
+    }
+    
+}
---
+++ InstanceVariable.java
@@ -0,0 +1,19 @@
+public class InstanceVariable {
+
+    public int age;
+
+    InstanceVariable(int age){
+        this.age=age;
+    }
+
+    public static void main (String[] args){
+        InstanceVariable x = new InstanceVariable(10);
+
+        System.out.println("Age of variable = "+ x.age);
+    }
+    
+}
+
+
+
+// https://www.geeksforgeeks.org/java/java-keywords/
